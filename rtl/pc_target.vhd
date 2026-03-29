ca6f11190d2e53779cbb43f298a9eb51447588db
usman1515 <usmansiddique1515@gmail.com>
2026-03-29 23:51:37 +0500

    feat: added rtl block pc_target

---
+++ rtl/pc_target.vhd
@@ -0,0 +1,44 @@
+-- ////////////////////////////////////////////////////////////////////////////////
+-- Company:
+-- Engineer:
+--
+-- Design Name:
+-- Module Name: pc_target
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- //////////////////////////////////////////////////////////////////////////////////
+
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+library work;
+use work.rtl_components.all;
+
+
+
+entity pc_target is
+    port (
+        i_pc_src_a: in std_logic_vector(31 downto 0);
+        i_imm_ext: in std_logic_vector(31 downto 0);
+        o_pc_plus4: out std_logic_vector(31 downto 0)
+    );
+end entity pc_target;
+
+architecture Behavioral of pc_target is
+
+begin
+
+    o_pc_plus4 <= std_logic_vector(unsigned(i_imm_ext) + unsigned(i_pc_src_a));
+
+end architecture Behavioral;
+
