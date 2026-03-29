ad7d1544f1dfae4b7f895b6be6a7835f18094792
usman1515 <usmansiddique1515@gmail.com>
2026-03-29 23:51:32 +0500

    feat: added rtl block pc_next

---
+++ rtl/pc_next.vhd
@@ -0,0 +1,43 @@
+-- ////////////////////////////////////////////////////////////////////////////////
+-- Company:
+-- Engineer:
+--
+-- Design Name:
+-- Module Name: pc_next
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- //////////////////////////////////////////////////////////////////////////////////
+
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+library work;
+use work.rtl_components.all;
+
+
+
+entity pc_next is
+    port (
+        i_pc: in std_logic_vector(31 downto 0);
+        o_pc_plus4: out std_logic_vector(31 downto 0)
+    );
+end entity pc_next;
+
+architecture Behavioral of pc_next is
+
+begin
+
+    o_pc_plus4 <= std_logic_vector(unsigned(i_pc) + x"0000_0004");
+
+end architecture Behavioral;
+
