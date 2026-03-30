94f371d474fb86e3e6a01b73ea3af046ff57e909
usman1515 <usmansiddique1515@gmail.com>
2026-03-30 18:21:13 +0500

    feat: added rtl block top_riscv_scp

---
+++ rtl/top_riscv_scp.vhd
@@ -0,0 +1,41 @@
+-- ////////////////////////////////////////////////////////////////////////////////
+-- Company:
+-- Engineer:
+--
+-- Design Name:
+-- Module Name: mux_2x1
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- //////////////////////////////////////////////////////////////////////////////////
+
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+library work;
+use work.rtl_components.all;
+
+
+entity top_riscv_scp is
+    port (
+        clk: in std_logic;
+        rst_n: in std_logic
+    );
+end entity top_riscv_scp;
+
+architecture Behavioral of top_riscv_scp is
+    
+begin
+    
+    
+    
+end architecture Behavioral;
