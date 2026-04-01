d85c48add07b632fd2ca84cc524c821164b8511b
usman1515 <usmansiddique1515@gmail.com>
2026-04-02 00:25:16 +0500

    fix: fixed syntax issues in tb_alu
    fix: fixed syntax issues in tb
    
    fix: fixed syntax issues in tb

--- tb/tb_alu.sv
+++ tb/tb_alu.sv
@@ -37,28 +37,26 @@ module alu_tb;
 
     task display_result;
         begin
-            #1 display("Time: %0t | Op: %b | A: %h | B: %h | Res: %h | Zero: %b",
+            #1 $display("Time: %3t | i_alu_control: %2d | i_alu_srcA: %8h | i_alu_srcB: %8h | o_alu_result: %8h | zero: %0b",
                 $time, i_alu_control, i_alu_srcA, i_alu_srcB, o_alu_result, o_zero);
         end
     endtask
 
     initial begin
-        // testing all modes
-        $display("--- Starting ALU Sequential Test ---");
-        for (int i=0; i<=16; i++) begin
-            i_alu_control = i[4:0];
-            i_alu_srcA = $urandom_range(32'h0, 32'hffff_ffff);
-            i_alu_srcB = $urandom_range(32'h0, 32'hffff_ffff);
+
+        $display("--- Testing all ALU modes sequentially ---");
+        for (int i=0; i<=31; i++) begin
+            i_alu_control = i;
+            i_alu_srcA = $urandom();
+            i_alu_srcB = $urandom();
             display_result();
         end
 
-        $display("\n--- Starting ALU Random Test ---");
-
-        // random test loop
+        $display("--- Testing ALU modes randomly ---");
         repeat(32) begin
-            i_alu_control = $urandom_range(5'h0, 5'h1f);
-            i_alu_srcA = $urandom_range(32'h0, 32'hffff_ffff);
-            i_alu_srcB = $urandom_range(32'h0, 32'hffff_ffff);
+            i_alu_control = $urandom_range(0, 17);
+            i_alu_srcA = $urandom();
+            i_alu_srcB = $urandom();
             display_result();
         end
 
